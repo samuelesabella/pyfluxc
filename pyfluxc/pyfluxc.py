c47c06530e8e47f9479f4dd14803eaf360838c6c
samuelesabella <samueleunipi@gmail.com>
2020-04-03 15:43:46 +0200

    bug fix

--- pyfluxc/pyfluxc.py
+++ pyfluxc/pyfluxc.py
@@ -51,7 +51,7 @@ class FluxResponse():
         dframe = pd.DataFrame(table, columns=keys)
         # Casting types ..... #
         for (k, dtype) in zip(keys, dtypes):
-            dframe.loc[:, k] = self.castFluxSeries(dframe[k], dtype)
+            dframe.loc[:, k] = self.castFluxSeries(dframe[k].copy(), dtype)
         # Grouping ..... #
         if groupby:
             groups = csv_data[1][3:]
